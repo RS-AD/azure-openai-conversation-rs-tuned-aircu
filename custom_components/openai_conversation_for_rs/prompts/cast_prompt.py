f68db4f7884c001bd13fd250553bef3f002eee74
mojokb <amaramusic@gmail.com>
2025-07-01 12:22:32 +0900

    refactor: Update cast prompt to improve clarity and add new response guidelines in Korean

--- custom_components/openai_conversation_for_rs/prompts/cast_prompt.py
+++ custom_components/openai_conversation_for_rs/prompts/cast_prompt.py
@@ -1,10 +1,12 @@
 CAST_PROMPT = """
-# Chromecast Control
+# Chromecast Controler
 ## Rules
 - If you think user wants to use youtube app, just say "googlecast_domain_flg" to let the system know that the user wants to use youtube app. And next user query also can be related to youtube app. In that case, you can say "googlecast_domain_flg" again.
 - If you think user wants to use netflix app, just say "googlecast_domain_flg" to let the system know that the user wants to use netflix app. And next user query also can be related to netflix app. In that case, you can say "googlecast_domain_flg" again.
 - When the user says:
   - "Search for ** on Netflix",
   - "Search for ** on Youtube",
-  - 또는 한국어로 "넷플릭스에서 ** 찾아줘", "넷플릭스에서 ** 틀어줘", "유튜브로 ** 검색해줘", "유튜브에서 ** 틀어줘" 처럼 말하면 이 경우에도 "googlecast_domain_flg" 응답하세요.
+  - 또는 한국어로 "넷플릭스에서 ** 찾아줘", "넷플릭스에서 ** 틀어줘", "유튜브로 ** 검색해줘", "유튜브에서 ** 틀어줘" 처럼 말하면 이 경우에도 "googlecast_domain_flg"로만 응답하세요.
+- 사용자의 명령이 크롬케스트를 이용하는 것이라면 "googlecast_domain_flg"로만 응답하세요.
+- 사용자의 명령이 동영상 재생에 관련된 내용이라면 "googlecast_domain_flg"로만 응답하세요.
 """
