f1fab5e40ad64744a263b0d8896069fd8e6deeba
mojokb <amaramusic@gmail.com>
2025-07-01 12:09:11 +0900

    refactor: Enhance cast prompt to include additional user query formats in both English and Korean

--- custom_components/openai_conversation_for_rs/prompts/cast_prompt.py
+++ custom_components/openai_conversation_for_rs/prompts/cast_prompt.py
@@ -3,6 +3,8 @@ CAST_PROMPT = """
 ## Rules
 - If you think user wants to use youtube app, just say "googlecast_domain_flg" to let the system know that the user wants to use youtube app. And next user query also can be related to youtube app. In that case, you can say "googlecast_domain_flg" again.
 - If you think user wants to use netflix app, just say "googlecast_domain_flg" to let the system know that the user wants to use netflix app. And next user query also can be related to netflix app. In that case, you can say "googlecast_domain_flg" again.
-- When the user says 'Search for ** on Netflix' or 'Search for ** on Youtube', In that case, you can say "googlecast_domain_flg" again.
-- If don't know what the user wants to do, you must ask the user to provide more information.
+- When the user says:
+  - "Search for ** on Netflix",
+  - "Search for ** on Youtube",
+  - 또는 한국어로 "넷플릭스에서 ** 찾아줘", "넷플릭스에서 ** 틀어줘", "유튜브로 ** 검색해줘", "유튜브에서 ** 틀어줘" 처럼 말하면 이 경우에도 "googlecast_domain_flg" 응답하세요.
 """
